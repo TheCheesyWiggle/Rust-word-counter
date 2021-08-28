0a3bda82a7f3dec53a46fc694f8708a3fe71974a
TheCheesyWiggle <74103207+TheCheesyWiggle@users.noreply.github.com>
2021-08-28 18:17:28 +0100

    counts how many times a word is repeated in the file maybe add a sorting algorithm?

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ use std::fs::File;
 use std::io::prelude::BufRead;
 use std::io::BufReader;
 use std::collections::HashMap;
+use std::path::Path;
 
 #[derive(Debug)]
 struct WordCounter(HashMap<String,u64>);
 
 impl WordCounter{
     fn new() -> WordCounter {
-        WordCounter(Hashmap::new());
+        WordCounter(HashMap::new())
     }
-    fn increment(word: &str){
+    fn increment(&mut self, word: &str){
         let key = word.to_string();
         let count = self.0.entry(key).or_insert(0);
         *count += 1
@@ -21,22 +22,21 @@ impl WordCounter{
     fn display(self){
         for (key, value) in self.0.iter()
         {
-            println!("{},{}",key,value);
+            println!("{}:{}",value,key);
         }
     }
 }
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    let filename = arguments[1];
-    println!("Processing file: {}",filename);
-    let file = file::open(filename).expect("Could not open file");
+    let filename = Path::new("./Test.txt");
+    println!("Processing file: {:?}",filename);
+    let file= File::open(filename).expect("Could not open file");
     let reader = BufReader::new(file);
     let mut word_counter = WordCounter::new();
 
     for line in reader.lines(){
         let line = line.expect("Could not read line");
         let words = line.split(" ");
-        for words in words{
+        for word in words{
             if word == ""{
                 continue
             }
